5e64dd50fdc0d4111d21f6a637ff9d9cf6cf099b
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-09-29 14:26:22 +0300

    feat: update aplication status

--- strides/aplications/models.py
+++ strides/aplications/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class ApplicationStatus(models.TextChoices):
+    DRAFT = 'draft', 'Черновик'
+    SUBMITTED = 'submitted', 'Отправлена'
+    REVIEW = 'review', 'Необходима проверка'
+    APPROVED = 'approved', 'Одобрена'
+    REJECTED = 'rejected', 'Отклонена'
+
+
 class Aplication(models.Model):
     """
     Заявки пользователя.
@@ -14,6 +22,11 @@ class Aplication(models.Model):
         related_name='aplications'
     )
     json_data = models.JSONField(null=True, blank=True)
-    is_finished = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=20,
+        choices=ApplicationStatus.choices,
+        default=ApplicationStatus.DRAFT,
+        db_index=True,
+    )
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
