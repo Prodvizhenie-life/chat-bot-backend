f9c5e8baf6f022bb4829122311cb453cffdd5544
Vlad Panenko <vlad593@mail.ru>
2025-09-30 14:51:04 +0400

    Fix admin.py for Users app

--- strides/users/admin.py
+++ strides/users/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from .models import User
+from .models import ProdvizhenieLifeUser
 
 
-@admin.register(User)
+@admin.register(ProdvizhenieLifeUser)
 class UserAdmin(DjangoUserAdmin):
-    model = User
-    list_display = ("username", "full_name", "email", "role", "is_staff", "is_superuser")
+    model = ProdvizhenieLifeUser
+    list_display = ("username", "fio", "email", "role", "is_staff", "is_superuser")
     fieldsets = DjangoUserAdmin.fieldsets + (
-        (None, {"fields": ("full_name", "role")}),
+        (None, {"fields": ("fio", "role")}),
     )
